d83b6957b800bb9d9d250ad7ab068c92cdf8c6a4
soulAlexa <volertt48@gmail.com>
2024-08-09 02:14:09 +0300

    #1

--- main.py
+++ main.py
@@ -3,8 +3,6 @@
 # import json
 # from base64 import b64decode
 #
-# Api_cey = ""
-# openai.api_key = Api_cey
 #
 # prompt = "capital letter h in black on white background"
 # response = openai.Image.create(
